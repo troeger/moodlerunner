5a51488be60df3ea7cf6ad804261aa907fa20a05
Peter Tröger <peter@troeger.eu>
2019-03-19 18:50:50 +0100

    First working version

--- moodlerunner.py
+++ moodlerunner.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 
-from moodleteacher import MoodleConnection, MoodleAssignments, MoodleSubmissionFile
+from moodleteacher.connection import MoodleConnection
+from moodleteacher.courses import MoodleCourse
+from moodleteacher.jobs import ValidationJob
 import logging
 import os
+import time
+import sys
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+# Enable library debug logging on screen
+logging.getLogger('moodleteacher').addHandler(handler)
+logger = logging.getLogger('moodlerunner')
+logger.addHandler(handler)
 
 
 def check_env_var(name, mandatory, default=None):
@@ -19,41 +29,58 @@ def check_env_var(name, mandatory, default=None):
         return os.environ[name]
 
 
-def handle_submission(sub):
-    files = MoodleSubmissionFile.from_urls(conn, sub.files)
-    logger.debug("Submission {0} has {1} files.".format(sub.id, len(files)))
-    # TODO: Unpack submission in temp directory
-
-    # TODO: Call validator function, provide MoodleSubmission object
-
-    if finish == 'submission':
-        exit(0)
-
-
 if __name__ == '__main__':
     url = check_env_var('RUNNER_URL', mandatory=True)
     key = check_env_var('RUNNER_KEY', mandatory=True)
-    # choices: ['submission', 'assignment', 'never']
-    finish = check_env_var('RUNNER_FINISH', mandatory=False, default='never')
+    course_id = check_env_var('RUNNER_COURSE_ID', mandatory=True)
+    course_id = int(course_id)
+    folder_id = check_env_var('RUNNER_FOLDER_ID', mandatory=True)
+    folder_id = int(folder_id)
+    log_level = check_env_var('RUNNER_LOG_LEVEL', mandatory=False)
+    if log_level:
+        log_level = logging.getLevelName(log_level)
+    else:
+        log_level = logging.DEBUG
+    logger.setLevel(log_level)
+
+    # choices: ['submission', 'assignment', 'cycleseconds_XXX']
+    mode = check_env_var('RUNNER_MODE', mandatory=False, default='cycleseconds_30')
 
     logger.debug("Connecting to Moodle at {0} ...".format(url))
     conn = MoodleConnection(moodle_host=url,
                             token=key, interactive=False)
 
-    assignments = MoodleAssignments(conn)
+    while(True):
+        # Get assignments for this course from Moodle server
+        course = MoodleCourse.from_course_id(conn, course_id)
+        assignments = course.assignments()
+        logger.info("Course {0} with {1} assignments".format(course, len(assignments)))
 
-    for assignment in assignments:
-        logger.debug("Checking assignment '{0}' ...".format(assignment.name))
+        # Get folder with validation scripts from Moodle server
+        for folder in course.get_folders():
+            if folder.id_ == folder_id:
+                validators_folder = folder
+                logger.info("Folder with validators: {0}".format(validators_folder))
 
-        if not assignment.course.can_grade:
-            logger.error("Assignment is not gradable.")
+        # Scan validator files in folder, determine according assignment and check if it has submissions
+        for validator in validators_folder.files:
+            validator_assignment_name = validator.name.split('.')[0]
+            for assignment in assignments:
+                if assignment.name.strip().lower() == validator_assignment_name.strip().lower():
+                    submissions = assignment.submissions()
+                    logger.info("Found assignment {0} with {1} submissions.".format(assignment, len(submissions)))
+                    for submission in submissions:
+                        logger.info("Submission to be validated: {0}".format(submission))
+                        job = ValidationJob(submission, validator)
+                        # Note: Log level for moodleteacher library is set to the same value as here
+                        job.start(log_level=log_level)
+                        if mode == 'submission':
+                            exit(0)
+                if mode == 'assignment':
+                    exit(0)
+        if mode.startswith('cycleseconds_'):
+            timeout = int(mode.split('_')[1])
+            logger.debug("Sleeping for {0} seconds ...".format(timeout))
+            time.sleep(timeout)
         else:
-            logger.debug("Fetching validation script ...")
-            # TODO: Use API to fetch validator file
-
-            submissions = assignment.submissions()
-            for sub in submissions:
-                handle_submission(sub)
-
-        if finish == 'assignment':
             exit(0)
