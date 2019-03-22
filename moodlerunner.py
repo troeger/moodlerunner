6d0999dc92f62516a3203ff42adf4eda46a4bf6f
Peter Tröger <peter@troeger.eu>
2019-03-22 15:01:32 +0100

    Add support for local testing, based on moodleteacher 0.1.2

--- moodlerunner.py
+++ moodlerunner.py
@@ -3,6 +3,11 @@
 from moodleteacher.connection import MoodleConnection
 from moodleteacher.courses import MoodleCourse
 from moodleteacher.validation import Job
+from moodleteacher.files import MoodleFile
+from moodleteacher.submissions import MoodleSubmission
+from moodleteacher.assignments import MoodleAssignment
+
+
 import logging
 import os
 import time
@@ -10,7 +15,8 @@ import sys
 
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 # Enable library debug logging on screen
 logging.getLogger('moodleteacher').addHandler(handler)
@@ -29,7 +35,28 @@ def check_env_var(name, mandatory, default=None):
         return os.environ[name]
 
 
+def test_mode(validator_fname, submission_fname):
+    conn = MoodleConnection(is_fake=True)
+    course = MoodleCourse(conn=conn, course_id=1)
+    assignment = MoodleAssignment(
+        course=course, assignment_id=1, allows_feedback_comment=True)
+    submission = MoodleSubmission.from_local_file(
+        assignment=assignment, fpath=submission_fname)
+    validator = MoodleFile.from_local_file(validator_fname)
+    job = Job(submission, validator, preamble="Local test result: ")
+    job.start(log_level=logging.DEBUG)
+
+
 if __name__ == '__main__':
+    # Check for local testing mode, needs no configuration
+    if len(sys.argv) > 1 and sys.argv[1] == 'test':
+        logger.info("Running in local test mode ...")
+        if len(sys.argv) != 4:
+            print("Usage: docker run --mount type=bind,src=$(PWD),dst=/hostdir troeger/moodlerunner:0.1.4 test <validator file> <submission file>")
+            exit(-1)
+        test_mode('/hostdir/' + sys.argv[2], '/hostdir/' + sys.argv[3])
+        exit(0)
+
     url = check_env_var('RUNNER_URL', mandatory=True)
     key = check_env_var('RUNNER_KEY', mandatory=True)
     course_id = check_env_var('RUNNER_COURSE_ID', mandatory=True)
@@ -45,7 +72,8 @@ if __name__ == '__main__':
     logger.setLevel(log_level)
 
     # choices: ['submission', 'assignment', 'cycleseconds_XXX']
-    mode = check_env_var('RUNNER_MODE', mandatory=False, default='cycleseconds_30')
+    mode = check_env_var('RUNNER_MODE', mandatory=False,
+                         default='cycleseconds_30')
 
     logger.info("Connecting to Moodle at {0} ...".format(url))
     conn = MoodleConnection(moodle_host=url,
@@ -55,13 +83,15 @@ if __name__ == '__main__':
         # Get assignments for this course from Moodle server
         course = MoodleCourse.from_course_id(conn, course_id)
         assignments = course.assignments()
-        logger.info("Course {0} with {1} assignments".format(course, len(assignments)))
+        logger.info("Course {0} with {1} assignments".format(
+            course, len(assignments)))
 
         # Get folder with validation scripts from Moodle server
         for folder in course.get_folders():
             if folder.id_ == folder_id:
                 validators_folder = folder
-                logger.info("Folder with validators: {0}".format(validators_folder))
+                logger.info("Folder with validators: {0}".format(
+                    validators_folder))
 
         # Scan validator files in folder, determine according assignment and check if it has submissions
         for validator in validators_folder.files:
@@ -69,14 +99,17 @@ if __name__ == '__main__':
             for assignment in assignments:
                 if assignment.name.strip().lower() == validator_assignment_name.strip().lower():
                     submissions = assignment.submissions()
-                    logger.info("Assignment {0} with {1} submissions has validators.".format(assignment, len(submissions)))
+                    logger.info("Assignment {0} with {1} submissions has validators.".format(
+                        assignment, len(submissions)))
                     for submission in submissions:
                         # Check if submission was already validated, based on preamble
                         current_feedback = submission.load_feedback()
                         if current_feedback and current_feedback.startswith(preamble):
-                            logger.info("Submission {0} was already validated, found preamble. Skipping it.".format(submission.id_))
+                            logger.info("Submission {0} was already validated, found preamble. Skipping it.".format(
+                                submission.id_))
                         else:
-                            logger.info("Submission to be validated: {0}".format(submission))
+                            logger.info(
+                                "Submission to be validated: {0}".format(submission))
                             job = Job(submission, validator, preamble=preamble)
                             # Note: Log level for moodleteacher library is set to the same value as here
                             job.start(log_level=log_level)
